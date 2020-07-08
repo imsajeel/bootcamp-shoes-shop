3b27b68617908509ce9c1ad55c7c9b566f4f3518
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-07-08 14:58:07 +0500

    Product Page Added

--- src/MyAppRoute.js
+++ src/MyAppRoute.js
@@ -8,7 +8,6 @@ import AllProducts, {
   ProductIndex,
   Product,
 } from "./Components/Pages/AllProducts";
-import Launch, { LaunchIndex, LaunchShoe } from "./Components/Pages/Launch";
 import NotFound from "./Components/Pages/NotFound";
 
 export default function MyAppRoute() {
@@ -17,10 +16,6 @@ export default function MyAppRoute() {
       <TopNav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="launch" element={<Launch />}>
-          <Route path="/" element={<LaunchIndex />} />
-          <Route path=":id" element={<LaunchShoe />} />
-        </Route>
         <Route path="products" element={<AllProducts />}>
           <Route path="/" element={<ProductIndex />} />
           <Route path=":id" element={<Product />} />
